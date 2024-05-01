67e3fa7491d634e5f72a16d344edbabccb9253d9
prot-alx <prot.alx.92@gmail.com>
2024-05-01 22:03:45 +0500

    first DOM req

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,14 +8,25 @@ const Dialogs = (props) => {
     let dialogsItems = props.state.dialogsData.map(dialogCompanion => <DialogItem name={dialogCompanion.name} id={dialogCompanion.id}/>);
 
     let messageItems = props.state.messagesData.map(lastMessage => <Message message={lastMessage.message}/>);
+    let newMessageElement = React.createRef();
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+     }
 
     return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogsItems}>
-                {dialogsItems}               
+        <div className={classes.dialogsWrapper}>
+            <div className={classes.dialogs}>
+                <div className={classes.dialogsItems}>
+                    {dialogsItems}               
+                </div>
+                <div className={classes.messages}>
+                    {messageItems}
+                </div>            
             </div>
-            <div className={classes.messages}>
-                {messageItems}
+            <div className={classes.sendMessage}>
+                <textarea ref={newMessageElement}></textarea>
+                <button onClick={sendMessage}>Send</button>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,12 +6,23 @@ const MyPosts = (props) => {
     
     let postsElements = props.postsData.map(post => <Post message={post.message} likesCount={post.likesCount} id={post.id}/>);
 
-    return (        
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    };
+
+    return (
         <div className={classes.postBlock}>
             <h3>My posts</h3>
             <div>
-                <div className={classes.textWrapper}><textarea></textarea></div>
-                <div><button>Send</button></div>
+                <div className={classes.textWrapper}>
+                    <textarea ref={newPostElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addPost}>Send</button>
+                </div>
             </div>
             <div className={classes.posts}>
                 {postsElements}
