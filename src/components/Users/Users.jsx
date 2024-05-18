aa6153e87c1b0d85b8c1b8dfd65afeb77daf387e
prot-alx <prot.alx.92@gmail.com>
2024-05-18 15:50:54 +0500

    pagination + arr slice

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,38 +5,68 @@ import userPhoto from '../../img/4c37ee25c50fe7f61ddc8858b0298042.jpg'
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items);
-            console.log(response.data.items);
-        })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+        });
     }
 
     render() {
+
+        let pagesCount = Math.ceil( this.props.totalUsersCount / this.props.pageSize );
+
+        let pages = [];
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
+        let curP = this.props.currentPage;
+        let curPF = ((curP - this.props.pageSize) < 0) ? 0 : curP - this.props.pageSize;
+        let curPL = curP + this.props.pageSize;
+        let slicedPages = pages.slice(curPF, curPL);
+
         return (
             <div>
-                {this.props.users.map(u =>
-                    <div key={u.id} className={styles.userStatus}>
-                        <span className={styles.block1}>
-                            <div>
-                                <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt='user ava' />
-                            </div>
-                            <div>
-                                {u.followed
-                                    ? <button className={`${styles.unfollowButton} ${styles.fButton}`} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button className={`${styles.followButton} ${styles.fButton}`} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                            </div>
-                        </span>
-                        <span className={styles.block2}>
-                            <span className={styles.userInfo}>
-                                <div className={styles.userName}>{u.name}:</div>
-                                <div className={styles.userStatusText}>{u.status != null ? u.status : 'some status text KJHASKDJHKAJSD'}</div>
+                <div className={styles.pagesList}>
+                    {slicedPages.map(p => 
+                        <span onClick={()=> { this.onPageChanged(p) }} key={p} className={this.props.currentPage === p ? styles.selectedPage : styles.nonselectedPage }>{p}</span>
+                    )}
+                </div>
+                <div className={styles.userStatuses}>
+                    {this.props.users.map(u =>
+                        <div key={u.id} className={styles.userStatus}>
+                            <span className={styles.block1}>
+                                <div>
+                                    <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt='user ava' />
+                                </div>
+                                <div>
+                                    {u.followed
+                                        ? <button className={`${styles.unfollowButton} ${styles.fButton}`} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                        : <button className={`${styles.followButton} ${styles.fButton}`} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                                </div>
                             </span>
-                            <span className={styles.location}>
-                                <div className={styles.userCountry}>{'u.location.country'},</div>
-                                <div className={styles.userCity}>{'u.location.city'}</div>
+                            <span className={styles.block2}>
+                                <span className={styles.userInfo}>
+                                    <div className={styles.userName}>{u.name}:</div>
+                                    <div className={styles.userStatusText}>{u.status != null ? u.status : 'some status text'}</div>
+                                </span>
+                                <span className={styles.location}>
+                                    <div className={styles.userCountry}>{'u.location.country'},</div>
+                                    <div className={styles.userCity}>{'u.location.city'}</div>
+                                </span>
                             </span>
-                        </span>
-                    </div>)}
+                        </div>)}
+                    </div>
             </div>
         )
     }
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,12 +1,14 @@
-//import {v1} from 'uuid';
-
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: [
-    ]
+    users: [],
+    pageSize: 6,
+    totalUsersCount: 0,
+    currentPage: 1,
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -36,6 +38,10 @@ const usersReducer = (state = initialState, action) => {
             }
         case SET_USERS:
             return { ...state, users: action.users };
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage};
+        case SET_TOTAL_USERS_COUNT:
+            return {...state,  totalUsersCount: action.count}
         default:
             return state;
     }
@@ -44,5 +50,8 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({ type: FOLLOW, userID });
 export const unfollowAC = (userID) => ({ type: UNFOLLOW, userID });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
+
 
 export default usersReducer;
